ae6d0ad554f9f6254c556aacd7ba309fc36f9b68
kkasu <kkasu1992@gmail.com>
2023-07-11 09:23:40 +0900

    Provider #13

--- src/test/java/hello/core/scope/ProtoTypeSingletonProblemTest.java
+++ src/test/java/hello/core/scope/ProtoTypeSingletonProblemTest.java
@@ -1,8 +1,10 @@
 package hello.core.scope;
 
+import jakarta.inject.Provider;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -36,25 +38,27 @@ public class ProtoTypeSingletonProblemTest {
     @DisplayName("싱글톤에서 프로토타입 의존 관계 주입")
     void singletonWithPrototype() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+
         ClientBean clientBean1 = ac.getBean(ClientBean.class);
         int count1 = clientBean1.logic();
         Assertions.assertThat(count1).isEqualTo(1);
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        Assertions.assertThat(count2).isEqualTo(2);
+
+        Assertions.assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean; // 생성 시점에 주입
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private Provider<PrototypeBean> provider;
+//        private ObjectProvider<PrototypeBean> provider;
 
         public int logic() {
+            PrototypeBean prototypeBean = provider.get();
+            System.out.println("prototypeBean = " + prototypeBean);
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
